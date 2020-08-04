55e15691ee901e248a9faa9f06b1da0df7519dba
MillenaQueiroz <millena.queiroz08@gmail.com>
2020-08-04 18:07:46 -0300

    backend

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,47 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Menu';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carousel'
-import Footer from '../../components/Footer'
+import Carousel from '../../components/Carousel';
+//import Footer from '../../components/Footer';
+import categoriasRepository from '../../repositories/categorias';
+import PageDefault from '../../components/PageDefault';
 
 function Home() {
+
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
+  useEffect(() => {
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        console.log(categoriasComVideos[0].videos[0]);
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
-    <div style={{background: "#141414"}}>
-      <Menu />
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"Novo episódio do Diagrama com Renato Reno do Bicicleta Sem Freio. A gente conversou sobre o começo do Bicicleta, sobre a banda Black Drawing Chalks e sobre os trabalhos em paredes que eles andam fazendo ao redor do mundo."}
-      />
+      
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={"Novo episódio do Diagrama com Renato Reno do Bicicleta Sem Freio. A gente conversou sobre o começo do Bicicleta, sobre a banda Black Drawing Chalks e sobre os trabalhos em paredes que eles andam fazendo ao redor do mundo."}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+          );
+        }
 
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />      
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
 
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />      
-
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />      
-
-      <Footer />
-    </div>
+    </PageDefault>
+    
   );
 }
 
